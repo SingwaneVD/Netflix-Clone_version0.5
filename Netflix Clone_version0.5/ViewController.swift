95d4ad46886454d413c8545839233e8c79aaa61f
SingwaneVD <94643260+SingwaneVD@users.noreply.github.com>
2022-08-08 22:53:58 +0200

    Initial Commit

---
+++ Netflix Clone_version0.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone_version0.5
+//
+//  Created by IACD-024 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
